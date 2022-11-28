62aed741717d97edb7cb734f5464dd7ecf396c9d
준1 <98933515+Wizone06@users.noreply.github.com>
2022-11-28 20:29:44 +0900

    플레이어 공격
    플레이어 공격

--- Assets/Wonjun/Script/Player.cs
+++ Assets/Wonjun/Script/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed = 4;
+    [SerializeField] private float attackcool = 0.6f;
     Vector3 dir;
+
 
     private Rigidbody2D _rigid;
     public LayerMask a;
     public bool below;
+    public bool att1 = true;
+    public bool attDown = true;
 
 
     SpriteRenderer sp;
@@ -26,17 +30,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         below = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, a);
+        transform.position += dir * speed * Time.deltaTime;
 
         if (Input.GetKeyDown(KeyCode.Space)&& below == true)
         {
             _rigid.velocity = new Vector2(_rigid.velocity.x, 5f);
             StartCoroutine(Jump());
         }
+        attackcool += Time.deltaTime;
 
 
         float x = Input.GetAxisRaw("Horizontal");
         dir = new Vector3(x, 0, 0);
-        transform.position += dir * speed * Time.deltaTime;
         if (x < 0)
         {
             //¿ÞÂÊ
@@ -53,6 +58,25 @@ public class Player : MonoBehaviour
         {
             anim.SetBool("run", false);
         }
+
+        if (Input.GetMouseButtonDown(0) && below == true && attackcool >= 0.7f&& att1 == true)
+        {
+            anim.SetTrigger("Attack");
+            attackcool = 0;
+            attDown = false;
+            StartCoroutine(attackpos());
+        }
+        if (Input.GetMouseButtonDown(0) && below == false && attDown == true)
+        {
+            anim.SetBool("Jattack", true);
+            StartCoroutine(att());
+            _rigid.gravityScale = 5;
+        }
+        else if(below == true)
+        {
+            _rigid.gravityScale = 1;
+            anim.SetBool("Jattack", false);
+        }
     }
 
     IEnumerator Jump()
@@ -60,5 +84,18 @@ public class Player : MonoBehaviour
         anim.SetBool("Jump", true);
         yield return new WaitForSeconds(0.5f);
         anim.SetBool("Jump", false);
+    }
+    IEnumerator attackpos()
+    {
+        speed = 0;
+        yield return new WaitForSeconds(0.7f);
+        speed = 5;
+        attDown = true;
+    }
+    IEnumerator att()
+    {
+        att1 = false;
+        yield return new WaitForSeconds(0.7f);
+        att1 = true;
     }
 }
