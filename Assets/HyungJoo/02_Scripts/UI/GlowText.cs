69d8d26e9182acece840da058a3c734e1b9d5c29
dlalsdb <nacfson@gmail.com>
2022-11-29 09:02:09 +0900

    Start

---
+++ Assets/HyungJoo/02_Scripts/UI/GlowText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GlowText : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _glowText;
+
+    public void GlowingText()
+    {
+
+    }
+}
